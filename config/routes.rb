d20f839ccda90c0398f8d30f5ea598f15e0c8c45
Benny Wong <bwong337@gmail.com>
2015-12-14 14:48:04 -0600

    more placeholders and style updates

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   get 'dashboard' => 'dashboard#index'
   get 'about' => 'high_voltage/pages#show', id: 'about'
+  get 'faqs' => 'high_voltage/pages#show', id: 'faqs'
   get 'home' => 'high_voltage/pages#show', id: 'home'
 end
